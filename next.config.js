b6f75587b59e59b89a4112f5644741241a0dca64
duenas <roy_david0701@hotmail.com>
2025-09-03 15:19:44 -0700

    Add next.config.js 1756937984350

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['i.ibb.co', 'images.unsplash.com'],
+    unoptimized: true
+  },
+}
+
+module.exports = nextConfig
