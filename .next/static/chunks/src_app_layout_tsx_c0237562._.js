1f3c0f2611c8d94f50142bf5371f0957e872b995
duenas <roy_david0701@hotmail.com>
2025-09-03 15:06:19 -0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1756937178880

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9f7d0372._.css",
+  "static/chunks/node_modules_next_dist_b4697f70._.js"
+],
+    source: "dynamic"
+});
